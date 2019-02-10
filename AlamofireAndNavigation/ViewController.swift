7d192bba6ac6f2dbbd008fe9a14ff6afbb8e1dcf
Alaa CS <alaa.civilsoft@gmail.com>
2019-02-10 14:16:51 +0200

    Initial Commit

---
+++ AlamofireAndNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireAndNavigation
+//
+//  Created by Mai Hassan on 2/10/19.
+//  Copyright © 2019 Mai Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
